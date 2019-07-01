07335a4a569e6be328e001b6278f69523edbd0ee
shamiul hoque <shoq9876@gmail.com>
2019-07-01 13:29:42 +0600

    first commit

---
+++ test/test.js
@@ -0,0 +1,6 @@
+var request = require('supertest'); var app = require('../server.js')  
+ describe('GET /', function() 
+ {   it('displays "Hello World!"', 
+ function(done) {         
+  request(app).get('/').expect('Hello Word!', done);  
+ }); });  
