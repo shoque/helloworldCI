f08be650bd1a6955ad8479751c369359ffd47620
shamiul hoque <shoq9876@gmail.com>
2019-07-01 13:36:44 +0600

    fixed hello world

--- test/test.js
+++ test/test.js
@@ -2,5 +2,5 @@ var request = require('supertest'); var app = require('../server.js')
  describe('GET /', function() 
  {   it('displays "Hello World!"', 
  function(done) {         
-  request(app).get('/').expect('Hello Word!', done);  
+  request(app).get('/').expect('Hello World!', done);  
  }); });  
