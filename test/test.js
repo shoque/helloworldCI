786fd51009f9cddf0be3e909c343a99cf19c2891
shamiul hoque <shoq9876@gmail.com>
2019-07-01 13:47:24 +0600

    --exit added to mocha

--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
-var request = require('supertest'); var app = require('../server.js')  
- describe('GET /', function() 
- {   it('displays "Hello World!"', 
- function(done) {         
-  request(app).get('/').expect('Hello World!', done);  
- }); });  
+var request = require('supertest');
+var app = require('../server.js');
+
+describe('GET /', function () {
+    it('displays "Hello World!"',
+        function (done) {
+            request(app).get('/').expect('Hello World!', done);
+        });
+});  
